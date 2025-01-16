df5a94d232717860d15696d612a332b272b05697
Christopher B Ayers <Cbayers4555@gmail.com>
2025-01-16 13:34:28 +0000

    Adding integers

---
+++ new_commands/01_First_Program/first_program.c
@@ -0,0 +1,13 @@
+// First program
+
+#include <stdio.h>
+
+int main() {
+    int first_number = 5;
+    int second_number = -15;
+    int sum = first_number + second_number;
+    printf("The first number is %d\n", first_number);
+    printf("The second number is %d\n", second_number);
+    printf("%d + %d = %d", first_number, second_number, sum);
+    return 0;
+}
